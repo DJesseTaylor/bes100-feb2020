54d7b876e922e7d8fed63dd22343723e85356f48
Jesse Taylor <8312896+DJesseTaylor@users.noreply.github.com>
2020-02-25 15:22:30 -0500

    Almost done on tuesday

--- LibraryApi/Controllers/StatusController.cs
+++ LibraryApi/Controllers/StatusController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LibraryApi.Services;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,7 +19,39 @@ namespace LibraryApi.Controllers
             };
             return Ok(response);
         }
+        [HttpGet("employees/{employeeId:int:min(1)}/salary")]
+        public ActionResult GetEmployeeSalary(int employeeId)
+        {
+            return Ok($"Employee {employeeId} has a salary of $65000.");
+        }
+
+        [HttpGet("shoes")]
+        public ActionResult GetSomeShoes([FromQuery] string color = "All")
+        {
+            return Ok($"Getting you shoes of color {color}");
+        }
+
+        [HttpGet("whoami")]
+        public ActionResult WhoAmI([FromHeader(Name = "User-Agent")] string userAgent)
+        {
+            return Ok($"You are using {userAgent}");
+        }
+
+        [HttpPost("employees")]
+        public ActionResult AddAnEmployee([FromBody] NewEmployee employee, [FromServices] IGenerateEmployeeIds idGenerator)
+        {
+            var id = idGenerator.GetNewEmployeeId();
+            return Ok($"Hiring {employee.Name} starting at {employee.StartingSalary.ToString("c")} with id of {id.ToString()}");
+        }
     }
+
+
+    public class NewEmployee
+    {
+        public string Name { get; set; }
+        public decimal StartingSalary { get; set; }
+    }
+
 
     public class StatusResponse
     {
